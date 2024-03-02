d555e0d910f96b38185eeb382323c19a092d92d2
Christopher James L. Castillo <castillochristopherjames@gmail.com>
2024-03-02 23:20:08 +0800

    created class for logging service

--- src/services/log/log_handler.py
+++ src/services/log/log_handler.py
@@ -1,20 +1,31 @@
 import logging
 
-def create_logging_service(file_dir: str) -> logging.Logger:
-    logger = logging.getLogger(__name__)
+class LoggingService():
+    def __init__(self, file_dir:str):
+        common_format = logging.Formatter('[%(levelname)s] %(asctime)s | %(message)s')
 
-    # handlers
-    c_handler = logging.StreamHandler()
-    f_handler = logging.FileHandler('file.log')
-    c_handler.setLevel(logging.WARNING)
-    f_handler.setLevel(logging.ERROR)
+        # handlers
+        self.logger = logging.getLogger(__name__)
+        
+        self.c_handler = logging.StreamHandler()
+        self.f_handler = logging.FileHandler(file_dir)
+        
+        # set level
+        self.c_handler.setLevel(logging.WARNING)
+        self.f_handler.setLevel(logging.ERROR)
 
-    c_format = logging.Formatter('%(filename)s:%(funcName)s() - %(levelname)s - %(message)s')
-    f_format = logging.Formatter('%(filename)s:%(funcName)s() - %(levelname)s - %(message)s')
-    c_handler.setFormatter(c_format)
-    f_handler.setFormatter(f_format)
+        # set formatter to common format
+        self.c_handler.setFormatter(common_format)
+        self.f_handler.setFormatter(common_format)
+        
+        # add handlers
+        self.logger.addHandler(self.c_handler)
+        self.logger.addHandler(self.f_handler)
 
-    logger.addHandler(c_handler)
-    logger.addHandler(f_handler)
-
-    return logger
+    # logs warning
+    def log_warning(self, file_name:str, func_name:str, msg:str):
+        self.logger.warning(f'{file_name.split("/")[-1]}:{func_name}() - {msg}')
+        
+    # logs error
+    def log_error(self, file_name:str, func_name:str, msg:str):
+        self.logger.error(f'{file_name.split("/")[-1]}:{func_name}() - {msg}')
