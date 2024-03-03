5a8e5be01c7596ba8112b5c799c03db34e30d8ff
Christopher James L. Castillo <castillochristopherjames@gmail.com>
2024-03-03 22:22:28 +0800

    created jwt user getter

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,11 +1,19 @@
+# imported libraries
 from typing import Annotated
-from fastapi import Header, HTTPException
-
+from fastapi import Header, HTTPException, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from datetime import datetime
+from jose import jwt
+from pydantic import ValidationError
+import os
+# user generated
 from services.db.database import Session
+from services.db.models import User
 from services.storage.cache_handler import CacheHandler
 from services.email.email_handler import EmailHandler
 from services.log.log_handler import LoggingService
 from services.aws.s3_handler import S3_Handler
+from models.auth_details import AuthDetails
 
 # dependencies go here
 
@@ -24,4 +32,39 @@ def get_logger():
 def get_s3_handler():
     return S3_Handler()
 
-# add JWT support here
+reuseable_oauth = OAuth2PasswordBearer(
+        tokenUrl="/login",
+        scheme_name="JWT"
+    )
+
+# on a later date, try to place this on a separate python file
+async def get_current_user(token: str = Depends(reuseable_oauth)) -> AuthDetails:
+    try:
+        payload = jwt.decode(
+            token, os.environ['JWT_SECRET_KEY'], algorithms=['HS256']
+        )
+        
+        if datetime.fromtimestamp(payload['exp']) < datetime.now():
+            raise HTTPException(
+                status_code = status.HTTP_401_UNAUTHORIZED,
+                detail="Token expired",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except(jwt.JWTError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+    db = await get_db_session()
+    
+    user:User = db.query(User).filter(User.username==payload['sub']).first()
+    
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Could not find user",
+        )
+    
+    return AuthDetails(user.id, user.username, user.level)
