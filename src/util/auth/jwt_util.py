8fc57fbe972782b83974c4d171be604574f71a01
Christopher James L. Castillo <castillochristopherjames@gmail.com>
2024-03-03 22:18:47 +0800

    created util functins for jwt authentication/authorization

---
+++ src/util/auth/jwt_util.py
@@ -0,0 +1,42 @@
+import os
+from dotenv import load_dotenv
+from datetime import datetime, timedelta
+from typing import Union, Any
+from jose import jwt
+
+import bcrypt
+from base64 import b64decode, b64encode
+
+load_dotenv()
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30  # 30 minutes
+REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7 # 7 days
+ALGORITHM = "HS256"
+JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']   # should be kept secret
+JWT_REFRESH_SECRET_KEY = os.environ['JWT_REFRESH_SECRET_KEY']    # should be kept secret
+
+def get_hashed_password(password: str) -> str:
+    return bcrypt.hashpw(password)
+
+def verify_password(password: str, hashed_pass: str) -> bool:
+    return bcrypt.checkpw(password.encode('utf-8'), bytes(hashed_pass, 'UTF-8'))
+
+def create_access_token(username: Union[str, Any], role: Union[str, Any], expires_delta: int = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+    else:
+        expires_delta = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    to_encode = {"exp": expires_delta, "sub": str(username), "role":str(role)}
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, ALGORITHM)
+    return encoded_jwt
+
+def create_refresh_token(username: Union[str, Any], role: Union[str, Any], expires_delta: int = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+    else:
+        expires_delta = datetime.utcnow() + timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
+    
+    to_encode = {"exp": expires_delta, "sub": str(username), "role":str(role)}
+    encoded_jwt = jwt.encode(to_encode, JWT_REFRESH_SECRET_KEY, ALGORITHM)
+    return encoded_jwt
