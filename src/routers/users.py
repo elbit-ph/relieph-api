9e5fbf3df2de10a86b7a62d19148d085e3f137c6
Christopher James L. Castillo <castillochristopherjames@gmail.com>
2024-04-04 18:47:18 +0800

    created proper user upgrade mechanism

--- src/routers/users.py
+++ src/routers/users.py
@@ -2,7 +2,7 @@ from typing import Annotated, List
 from fastapi import APIRouter, Depends, UploadFile, HTTPException, status, Response, Body, Form
 from dependencies import get_db_session, get_logger, get_s3_handler, get_current_user
 from services.db.database import Session
-from services.db.models import User, Address
+from services.db.models import User, Address, UserUpgradeRequest
 from services.log.log_handler import LoggingService
 from services.aws.s3_handler import S3_Handler
 from models.auth_details import AuthDetails
@@ -36,73 +36,45 @@ class NewAddressDTO(BaseModel):
     coordinates: str
 
 @router.get("/")
-def retrieveUsers(db: DB, p: int = 1, c: int = 10):
-    return db.query(User).limit(c).offset((p-1)*c).all()
+async def retrieveUsers(db: DB, s3:S3Handler, p: int = 1, c: int = 10):
+    # truncate sensitive data - 
+    users = db.query(User.id, User.sponsor_id, User.first_name, User.last_name, User.level).limit(c).offset((p-1)*c).all() 
+    to_return = []
+    for user in users:
+        # append image link
+        profile_link = s3.get_image(user[0], 'users')
+        to_return.append({
+            "id" : user[0],
+            "sponsor_id" : user[1],
+            "first_name" : user[2],
+            "last_name" : user[3],
+            "level" : user[4],
+            "profile" : profile_link[0] if profile_link[1] != False else None
+        })
+    return to_return
 
 @router.get("/{id}")
-def retrieveUser(db:DB, id:int):
+async def retrieveUser(db:DB, id:int, s3:S3Handler):
     user:User = db.query(User).filter(User.id == id).first()
     if user is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="User not found."
         )
+    
+    profile_link = await s3.get_image(id, 'users')
+
+    if profile_link[1] == False:
+        profile_link[0] = 'other link'
+
     return {
         "id": user.id,
+        "sponsor_id" : user.sponsor_id,
         "firstname" : user.first_name,
-        "lastname" : user.last_name
+        "lastname" : user.last_name,
+        "level" : user.level,
+        "profile" : profile_link[0]
     }
-
-class CreateAnonymousUserDTO(BaseModel):
-    username:str
-    email:str
-
-@router.post("/anonymous")
-def createAnonymousUser(db:DB, body:CreateAnonymousUserDTO):
-    newUser = User()
-    newUser.username = body.username
-    newUser.email = body.email
-
-    # add user to db
-    db.add(newUser)
-    db.commit()
-
-    return {
-        "detail": "success"
-    }
-
-class UpgadeUserDTO(BaseModel):
-    fname: str
-    lname: str
-    password: str
-    confirmPassword: str
-    mobile: str
-
-    address:NewAddressDTO
-
-@router.post("/anonymous/{email}")
-def UpgradeUser(db:DB, email:str, res: Response, body: UpgadeUserDTO):
-    user:User = db.query(User).filter(User.email == email).first()
-
-    # check if user is in db to begin with
-    if user is None:
-        res.status_code = 400
-        return {"detail": "Non-existing user"}
-    
-    if body.password != body.confirmPassword:
-        res.status_code = 400
-        return {"detail": "Passwords don't match"}
-
-    user.first_name = body.fname
-    user.last_name = body.lname
-    user.password = get_hashed_password(body.password)
-    user.mobile = body.mobile
-    user.level = 1
-    user.updated_at = datetime.now()
-
-    db.commit()
-
-    return {"detail": "success"}
 
 class BasicUserDTO(BaseModel):
     fname: str
@@ -112,11 +84,9 @@ class BasicUserDTO(BaseModel):
     confirmPassword: str
     email: str
     mobile: str
-    # address input
-    address:NewAddressDTO
 
-@router.post("/basic")
-async def createBasicUser(db:DB, res: Response, profile: UploadFile, s3:S3Handler, body:Json = Form()):
+@router.post("/personal")
+async def regular_signup(db:DB, res: Response, profile: UploadFile, s3:S3Handler, body:Json = Form()):
     body:BasicUserDTO = json.loads(json.dumps(body), object_hook=lambda d: SimpleNamespace(**d))
 
     # check if email is already used  
@@ -162,20 +132,6 @@ async def createBasicUser(db:DB, res: Response, profile: UploadFile, s3:S3Handle
     db.add(user)
     db.commit()
 
-    newAddress = Address()
-    
-    newAddress.owner_id = user.id
-    newAddress.owner_type = "USER"
-    newAddress.region = body.address.region
-    newAddress.city = body.address.city
-    newAddress.brgy = body.address.brgy
-    newAddress.street = body.address.street
-    newAddress.zipcode = body.address.zipcode
-    newAddress.coordinates = body.address.coordinates
-
-    db.add(newAddress)
-    db.commit()
-
     resu = await s3.upload_single_image(profile, user.id, 'users')
 
     if resu[1] == False:
@@ -186,9 +142,140 @@ async def createBasicUser(db:DB, res: Response, profile: UploadFile, s3:S3Handle
         "detail" : "Account successfully created."
     }
 
+class UpgradeAccountDTO(BaseModel):
+    first_name :str
+    last_name :str
+    birthday: datetime
+    sex: str
+    accountno: str
+    id_type: str
+    # address info
+    region: str
+    city: str
+    brgy: str
+    street: str
+    zipcode: int
+    coordinates: str
+
+@router.post("/upgrades")
+async def upgrade_personal_account(db:DB, res: Response, valid_id: UploadFile, s3:S3Handler, body:Json = Form(), user: AuthDetails = Depends(get_current_user)):
+    authorize(user, 1, 1) # only accounts of level 1 can upgrade to personal+
+
+    body:UpgradeAccountDTO = json.loads(json.dumps(body), object_hook=lambda d: SimpleNamespace(**d))
+
+    # validate input
+    if is_image_valid(valid_id) == False:
+        res.status_code = 400
+        return {'detail' : 'Invalid image.'}
+
+    # create account upgrade request
+    upgrade_request = UserUpgradeRequest()
+    
+    upgrade_request.user_id = user.user_id
+    upgrade_request.first_name = body.first_name
+    upgrade_request.last_name = body.last_name
+    upgrade_request.birthday = body.birthday
+    upgrade_request.sex = body.sex
+    upgrade_request.accountno = body.accountno
+    upgrade_request.id_type = body.id_type
+
+    # save address
+    address = Address()
+
+    address.owner_id = user.user_id
+    address.owner_type = 'USER'
+    address.region = body.region
+    address.city = body.city
+    address.brgy = body.brgy
+    address.street = body.street
+    address.zipcode = body.zipcode
+    address.coordinates = body.coordinates
+
+    db.add(upgrade_request)
+    db.add(address)
+    db.commit()
+    
+    # save valid id
+    image_upload_res = await s3.upload_multiple([valid_id], user.user_id, f'valid_ids/{user.user_id}')
+
+    return {'detail' : 'Successfully sent upgrade request.'}
+
+# get list of upgrade requests
+@router.get("/upgrades/")
+def retrieve_upgrade_requests(db:DB, p: int = 1, c: int = 10, status:str = 'ALL', user: AuthDetails = Depends(get_current_user)):
+    authorize(user, 4, 4) # only admin can see upgrade requests
+    
+    requests = List[UserUpgradeRequest]
+
+    match status.lower():
+        case 'pending':
+            requests = db.query(UserUpgradeRequest).filter(UserUpgradeRequest.status == 'PENDING').limit(c).offset((p-1)*c).all()
+        case 'rejected':
+            requests = db.query(UserUpgradeRequest).filter(UserUpgradeRequest.status == 'REJECTED').limit(c).offset((p-1)*c).all()
+        case 'approved':
+            requests = db.query(UserUpgradeRequest).filter(UserUpgradeRequest.status == 'APPROVED').limit(c).offset((p-1)*c).all()
+        case _:
+            requests = db.query(UserUpgradeRequest).limit(c).offset((p-1)*c).all()
+
+    return requests
+
+@router.get("/upgrades/{upgrade_request_id}")
+async def retrieve_upgrade_request(db:DB, s3:S3Handler, res:Response, upgrade_request_id: int, user: AuthDetails = Depends(get_current_user)):
+    authorize(user, 4, 4) 
+
+    request:UserUpgradeRequest = db.query(UserUpgradeRequest).filter(and_(UserUpgradeRequest.id == upgrade_request_id, UserUpgradeRequest.status == 'PENDING')).first()
+
+    if request is None:
+       res.status_code = 404
+       return {'detail' : 'Upgrade request not found.'}
+
+    to_return = {
+        'details' : request
+    }
+
+    # get images
+    image_res = await s3.retrieve_multiple(request.user_id, f'valid_ids/{user.user_id}')
+
+    if image_res[1] != False:
+        to_return['valid_id'] = image_res[0]
+
+    return to_return
+
+@router.post("/upgrades/{action}/{upgrade_request_id}")
+async def resolve_upgrade_request(db:DB, action:str, upgrade_request_id, res:Response, user: AuthDetails = Depends(get_current_user)):
+    authorize(user, 4, 4)
+
+    # check if requests exists
+    upgrade_request:UserUpgradeRequest = db.query(UserUpgradeRequest).filter(and_(UserUpgradeRequest.id == upgrade_request_id, UserUpgradeRequest.status == 'PENDING')).first()
+
+    if upgrade_request is None:
+        res.status_code = 404
+        return {'detail' : 'Upgrade request not found.'}
+    
+    user: User = db.query(User).filter(User.id == upgrade_request.user_id).first()
+
+    match action.lower():
+        case 'approve':
+            upgrade_request.status = 'APPROVED'
+            
+            # increment user id
+            user.level = 2
+        case 'reject':
+            upgrade_request.status = 'REJECTED'
+        case _:
+            res.status_code = 406
+            return {'detail' : 'Action not allowed.'}
+    upgrade_request.updated_at = datetime.now()
+
+    db.commit()
+
+    # send email to user
+
+    return {'detail' : 'Successfully resolved upgrade request.'}
+
 @router.patch("/address")
 async def editUserAddress(db:DB, res: Response, body: NewAddressDTO, user: AuthDetails = Depends(get_current_user)):#
-    authorize(user, 2,5)
+    authorize(user, 2,4)
 
     relief_address:Address = db.query(Address).filter(and_(Address.owner_type == 'USER', Address.owner_id == user.user_id)).first()
 
@@ -220,7 +307,7 @@ async def editUserAddress(db:DB, res: Response, body: NewAddressDTO, user: AuthD
 @router.post("/profile")
 async def saveUserProfile(image: UploadFile, res:Response, db:DB, s3:S3Handler, user: AuthDetails = Depends(get_current_user)):
     # get user id from authentication
-    authorize(2,5)
+    authorize(user, 1, 4)
 
     if is_image_valid(image) == False:
         res.status_code = 400
@@ -257,24 +344,9 @@ def retrieveUserProfile(id:int, res: Response, s3: S3Handler):
         'link': resu[0]
     }
 
-@router.post("/{id}/level/{to}")
-async def changeLevel(db:DB, id:int, to:int, user: AuthDetails = Depends(get_current_user)):
-    # check if user is admin
-    authorize(user, 5, 5)
-
-    # change level here
-    user:User = db.query(User).filter(User.id == id).first()
-    
-    # assign new level
-    user.level = to
-
-    return {
-        "detail": "Successfully changed level of user."
-    }
-
 @router.delete("/{id}")
 async def deleteUser(db:DB, id:int, res: Response, user:AuthDetails = Depends(get_current_user)):
-    authorize(user, 1,5)
+    authorize(user, 1,4)
     
     # check first if current user is similar to user
     if id != user.user_id and user.level != 5:
