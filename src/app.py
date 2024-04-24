c774bf9895df60bd730bb388cc093fbfea12fbf3
Christopher James L. Castillo <castillochristopherjames@gmail.com>
2024-04-24 22:31:12 +0800

    removed headliner -> moved to another api

--- src/app.py
+++ src/app.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
-from routers import auth, users, organizations, relief, foundations, volunteers, inkind, monetary, headlines, reports
-from util.headline_classifier.scheduler import scheduler
+from routers import auth, users, organizations, relief, foundations, volunteers, inkind, monetary, reports
 
 load_dotenv()
 
@@ -12,12 +11,8 @@ load_dotenv()
 #     'cache.lock_dir': '/tmp/cache/lock'
 # }
 
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     startup_event(background_tasks, db)
-#     yield
-
 api_app = FastAPI(title="info api")
+
 api_app.include_router(auth.router)
 api_app.include_router(users.router)
 api_app.include_router(organizations.router)
@@ -26,7 +21,6 @@ api_app.include_router(relief.router)
 api_app.include_router(foundations.router)
 api_app.include_router(inkind.router)
 api_app.include_router(monetary.router)
-api_app.include_router(headlines.router)
 api_app.include_router(reports.router)
 
 app = FastAPI(title="main app")
@@ -40,5 +34,3 @@ app.add_middleware(
 )
 
 app.mount("/api", api_app)
-
-scheduler.start()
