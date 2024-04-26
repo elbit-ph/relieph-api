0081ff19410363c63245d0ee8da83715ccc19c20
ChanMor <morelos.franzchristian@gmail.com>
2024-04-27 00:41:20 +0800

    Added generate relief effort template with inkind donation template and such using GEMINI API

---
+++ src/util/generate_relief/relief_integrity.py
@@ -0,0 +1,29 @@
+import json
+from gemini import model
+
+json_template = '''
+{
+    "relief_title": "",
+    "description": "",
+    "monetary_goal": "",
+    "inkind_donation": [
+        {
+            "item": "",
+            "item_desc": "",
+            "quantity": ""
+        }
+    ],
+    "deployment_date": "",
+}
+'''
+
+def relief_data(response):
+    jsonify_prompt = f"I need you to modify the keys in this JSON object {response}. Output strictly just the JSON object following this format {json_template}. Make sure to output a string in the"
+    relief_data_json = model.generate_content([jsonify_prompt])
+
+    data = relief_data_json.text.lstrip('```json')
+    data = data.rstrip('```')
+
+    relief_data_dict = json.loads(data) 
+    return relief_data_dict
+
---
+++ src/util/generate_relief/scheduler.py
@@ -0,0 +1,26 @@
+from pytz import utc
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.jobstores.memory import MemoryJobStore
+from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
+from .save import start_gen
+
+jobstore = MemoryJobStore()
+
+executors = {
+    'default': ThreadPoolExecutor(5),
+    'processpool': ProcessPoolExecutor(3)
+}
+job_defaults = {
+    'coalesce': False,
+    'max_instances': 3
+}
+
+scheduler_relief = BackgroundScheduler(
+    jobstores={'memory': jobstore},
+    executors=executors,
+    job_defaults=job_defaults,
+    timezone=utc
+)
+
+scheduler_relief.add_job(start_gen, 'interval', seconds=2400)
+
